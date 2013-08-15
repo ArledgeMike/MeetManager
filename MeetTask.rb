055f0c97794b82c36675aef6492668f3a6fdd142
Michael Arledge <arledgemike@gmail.com>
2013-08-15 19:51:57 -0400

    Update MeetTask.rb
    optimized task code

--- MeetTask.rb
+++ MeetTask.rb
@@ -1,50 +1,50 @@
 require 'MeetComment'
 
 class MeetTask
-attr_accessor :name, :task, :dueDate, :completed, :counter, :commentArray
+  
+attr_accessor :name, :task, :dueDate, :completed, :counter, :commentArray, :createdAt
 
 def initialize(idNumber, name, task, dueDate)
  @name = name
  @task = task
  @dueDate = dueDate
  @completed = false
- @counter = idNumber;
+ @createdAt = Time.new
  @commentArray = [];
- puts "New Task Succesfully Added"
+ puts "New Task Succesfully Added!"
 end
 
 def markComplete
  @completed = true
+ puts "This task is completed!" 
 end
 
 def taskInfo
- puts "This task belongs to #{name} they need to #{task} and have this completed by #{dueDate} is this task complete? #{@completed}"
- @commentArray.each { |comment| puts "Name: #{comment.commentorName} at #{comment.time} \n Commented: #{comment.comment}" }
+ puts "\n Task Owner: #{name} \n Created at: #{@createdAt} \n Task: #{task} \n Due Date: #{dueDate} \n Complete:  #{@completed} \n Number of Comments: #{@commentArray.size} \n Comments: \n"
+ @commentArray.each { |comment| puts "\n Name: #{comment.commentorName} \n Created At: #{comment.time} \n Comment: #{comment.comment}" }
 end
  
 def changeTaskOwner
- puts "Enter A New Owner For This Task\n"
+ print "Enter A New Owner For This Task\n>"
  newOwner = gets.strip
  @name = newOwner;
+ puts "Owner Transfered Succesfully!" 
 end
 
 def changeDueDate
- puts "Enter A New Due Date For This Task"
+ print "Enter A New Due Date For This Task\n>"
  newDate = gets.strip
  @dueDate = newDate
+ puts "Due Date Changed Succesfully!"
 end
 
 def comment
- puts "Enter Commentors Name"
+ print "Enter Commentors Name\n>"
  theCommentor = gets.strip
- puts "Enter A Comment \n"
+ print "Enter A Comment \n>"
  theComment = gets.strip
  aComment = MeetComment.new(theCommentor, theComment)
  @commentArray.push(aComment); 
 end
 
-
-
 end
-
-
