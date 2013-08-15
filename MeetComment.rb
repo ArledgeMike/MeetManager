6b5f71b9137a790d7b8681bc9ff470c1e2e4f0da
Michael Arledge <arledgemike@gmail.com>
2013-08-15 19:50:43 -0400

    Update MeetComment.rb
    optimized comment code

--- MeetComment.rb
+++ MeetComment.rb
@@ -1,14 +1,9 @@
 class MeetComment
 attr_accessor :commentorName, :comment, :time
-
-def initialize( name, comment)
- @commentorName = name
- @comment = comment
- @time = Time.new
- puts "Comment added successfully"
+  def initialize( name, comment)
+    @commentorName = name
+    @comment = comment
+    @time = Time.new
+    puts "Comment added successfully!"
+ end
 end
-
-
-end
-
-
