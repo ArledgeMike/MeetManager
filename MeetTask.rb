f46ad835b3f0a49702740fbe80603432a65c24cb
Michael Arledge <arledgemike@gmail.com>
2013-09-09 23:06:37 -0400

    Update to Meet Task
    Delete a comment added to Meet Task Object

--- MeetTask.rb
+++ MeetTask.rb
@@ -2,49 +2,59 @@ require 'MeetComment'
 
 class MeetTask
   
-attr_accessor :name, :task, :dueDate, :completed, :counter, :commentArray, :createdAt
 
-def initialize(idNumber, name, task, dueDate)
- @name = name
- @task = task
- @dueDate = dueDate
- @completed = false
- @createdAt = Time.new
- @commentArray = [];
- puts "New Task Succesfully Added!"
-end
+attr_accessor :name, :task, :dueDate, :completed, :createdAt, :commentArray
 
-def markComplete
- @completed = true
- puts "This task is completed!" 
-end
+  def initialize( name, task, dueDate, commentArray=[])
+    @name = name
+    @task = task
+    @dueDate = dueDate
+    @completed = false
+    @createdAt = Time.new
+    @commentArray = commentArray;
+    puts "New Meet Succesfully Added!"
+  end
 
-def taskInfo
- puts "\n Task Owner: #{name} \n Created at: #{@createdAt} \n Task: #{task} \n Due Date: #{dueDate} \n Complete:  #{@completed} \n Number of Comments: #{@commentArray.size} \n Comments: \n"
- @commentArray.each { |comment| puts "\n Name: #{comment.commentorName} \n Created At: #{comment.time} \n Comment: #{comment.comment}" }
-end
+  def markComplete
+    @completed = true
+    puts "This task is completed!" 
+  end
+
+  def taskInfo
+    puts "\n Task Owner: #{name} \n Created at: #{@createdAt} \n Task: #{task} \n Due Date: #{dueDate} \n Complete:  #{@completed} \n Number of Comments: #{@commentArray.size} \n Comments: \n"
+    @commentArray.each { |comment| puts "\n Name: #{comment.commentorName} \n Created At: #{comment.time} \n Comment: #{comment.comment}" }
+  end
  
-def changeTaskOwner
- print "Enter A New Owner For This Task\n>"
- newOwner = gets.strip
- @name = newOwner;
- puts "Owner Transfered Succesfully!" 
-end
+  def changeTaskOwner
+    print "Enter A New Owner For This Task\n>"
+    newOwner = gets.strip
+    @name = newOwner;
+    puts "Owner Transfered Succesfully!" 
+  end
 
-def changeDueDate
- print "Enter A New Due Date For This Task\n>"
- newDate = gets.strip
- @dueDate = newDate
- puts "Due Date Changed Succesfully!"
-end
+  def changeDueDate
+    print "Enter A New Due Date For This Task\n>"
+    newDate = gets.strip
+    @dueDate = newDate
+    puts "Due Date Changed Succesfully!"
+  end
 
-def comment
- print "Enter Commentors Name\n>"
- theCommentor = gets.strip
- print "Enter A Comment \n>"
- theComment = gets.strip
- aComment = MeetComment.new(theCommentor, theComment)
- @commentArray.push(aComment); 
-end
+  def add_comment
+    print "Enter Commentors Name\n>"
+    theCommentor = gets.strip.split(" ").map { |w| w.capitalize }.join(" ")
+    print "Enter A Comment \n>"
+    theComment = gets.strip
+    aComment = MeetComment.new(theCommentor, theComment)
+    @commentArray.push(aComment); 
+  end
+  
+  def delete_comment
+    @commentArray.each { |comment| puts "\n Name: #{comment.commentorName} \n Created At: #{comment.time} \n Comment: #{comment.comment}" }
+    print "Which Comment Would You Like To Delete?\n>"
+    comment_num = (gets.strip.to_i)
+    comment_num = comment_num - 1
+    @commentArray.delete_at(comment_num);
+    puts "That Meet's Comment Has Been Removed!"
+  end
 
 end
