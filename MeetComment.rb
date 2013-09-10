c7b1130fe93463c521dc2602ecb765a37ed5f2a8
Michael Arledge <arledgemike@gmail.com>
2013-09-09 23:13:29 -0400

    Update to Meet Comments
    cleaned up meet comment code

--- MeetComment.rb
+++ MeetComment.rb
@@ -1,9 +1,14 @@
 class MeetComment
+
 attr_accessor :commentorName, :comment, :time
-  def initialize( name, comment)
+  
+  def initialize(name, comment)
+
     @commentorName = name
     @comment = comment
     @time = Time.new
     puts "Comment added successfully!"
- end
+
+  end
+
 end
